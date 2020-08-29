03067dd012b0b631a93b09e6ca064b36b50448a7
Andrew Samuel <samueltandrew@gmail.com>
2020-08-28 19:17:06 -0500

    Solutions coded in Python

---
+++ Assignment_1/hw_1_q2.py
@@ -0,0 +1,7 @@
+#Question_2
+
+sales = float(input("Please enter projected amount of total sales"))
+
+profit = (sales * 0.23)
+
+print(f'The amount of profit to be made with the projected sales is {profit}')
