f94c511969ec04d162dc6027efe2f3e8e91ca500
Andrew Samuel <samueltandrew@gmail.com>
2020-08-28 19:37:42 -0500

    Delete hw_1_q3.py

--- Assignment_1/hw_1_q3.py
+++
@@ -1,8 +0,0 @@
-#Question_3
-
-sqft = float(input("Please enter the amount of square feet in this tract of land"))
-
-acres = (sqft/43560)
-
-print(f'The amount of acres in this tract is {acres}')
-
